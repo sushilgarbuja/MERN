e45dca8c059804eb3c4e3e4e8b13cd36d0fc182c
sushil <sushilgarbuja82335@gmail.com>
2025-01-20 14:52:13 +0545

    add blogschema

---
+++ model/blogModel.js
@@ -0,0 +1,25 @@
+const mongoose=require('mongoose')
+//create schema by using nosql orm in mongoose
+//schema is a class using pascal naming convention
+
+const Schema=mongoose.Schema
+
+// const blogSchema=new mongoose.Schema({})
+const blogSchema=new Schema({
+    title:{
+        type:String,
+        unique:true
+    },
+    subtitle:{
+        type:String
+    },
+    description:{
+        type:Text
+    },
+    image:{
+        type:String
+    }
+})
+//() cha vane method ho
+const Blog=mongoose.model('Blog',blogSchema)
+module.exports=Blog
