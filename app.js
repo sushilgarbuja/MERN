6629323b10cbccdc118dd5ffe807367616f63e73
sushil <sushilgarbuja82335@gmail.com>
2025-01-20 15:35:24 +0545

    successfully add data from postman

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ require('dotenv').config()
 const express=require('express')
 const connectToDatabase = require('./database')
 const app=express()
+app.use(express.json())
+
 
 connectToDatabase()
 
@@ -22,7 +24,12 @@ app.get("/about",(req,res)=>{
         message:"hello about"
     })
 })
-
+app.post("/blog",(req,res)=>{
+    console.log(req.body)
+    res.status(200).json({
+        message:"Blog api has been called"
+    })
+})
 app.listen(process.env.PORT,()=>{
     console.log("node js project has been started...")
 })
